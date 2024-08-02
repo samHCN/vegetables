46c9fe6e42a97991e39681dd889998c601f32fce
greenagile3dcloud <greenagile3dcloud@gmail.com>
2024-08-02 11:03:26 +0000

    Updated dependencies and API endpoint

--- main.py
+++ main.py
@@ -1,3 +1,55 @@
+from flask import Flask, request, jsonify, send_file, send_from_directory
+import json
+import os
+import google.generativeai as genai
+
+# 🔥🔥 FILL THIS OUT FIRST! 🔥🔥
+# Get your Gemini API key by:
+# - Selecting "Add Gemini API" in the "Project IDX" panel in the sidebar
+# - Or by visiting https://g.co/ai/idxGetGeminiKey
+API_KEY = 'AIzaSyAH17rAIUWAT17_jihVOZXiBUKbcrv2D4w'
+
+genai.configure(api_key=API_KEY)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return send_file('web/index.html')
+
+
+@app.route("/api/generate", methods=["POST"])
+def generate_api():
+    if request.method == "POST":
+        if API_KEY == 'TODO':
+            return jsonify({ "error": '''
+                To get started, get an API key at
+                https://g.co/ai/idxGetGeminiKey and enter it in
+                main.py
+                '''.replace('\n', '') })
+        try:
+            req_body = request.get_json()
+            content = req_body.get("contents")
+            model = genai.GenerativeModel(model_name=req_body.get("model"))
+            response = model.generate_content(content, stream=True)
+            def stream():
+                for chunk in response:
+                    yield 'data: %s\n\n' % json.dumps({ "text": chunk.text })
+
+            return stream(), {'Content-Type': 'text/event-stream'}
+
+        except Exception as e:
+            return jsonify({ "error": str(e) })
+
+
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory('web', path)
+
+
+if __name__ == "__main__":
+    app.run(port=int(os.environ.get('PORT', 9000)))
 from flask import Flask, request, jsonify
 from PIL import Image
 from io import BytesIO
